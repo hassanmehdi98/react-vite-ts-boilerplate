388d0c213d41d55205f9310bf0e9b40c0317643c
Hassan Mehdi <hassan.mehdi@koderlabs.com>
2022-09-07 16:53:28 +0500

    svg typing fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,8 @@
+declare module '*.svg' {
+    const content: React.FC<React.SVGProps<SVGElement>>
+    export default content
+}
+
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
